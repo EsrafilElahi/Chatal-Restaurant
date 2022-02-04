f37cc219788d75189dae4861cc056d57951ffc7a
Esrafil <57289402+EsrafilElahi@users.noreply.github.com>
2022-02-04 20:38:21 +0330

    primitive convert to pwa

--- js/app.js
+++ js/app.js
@@ -1,52 +1,60 @@
+window.onload = () => {
+  if ("serviceWorker" in navigator) {
+    navigator.serviceWorker
+      .register("../service-worker.js")
+      .then((res) => console.log("service worker registered", res))
+      .catch((err) => console.log("service worker not registered", err));
+  }
+};
 
-const hamburger = document.getElementsByClassName('fa-hamburger')[0];
-const nav = document.getElementsByTagName('nav')[0];
-const a = document.querySelectorAll('a');
-const dots1 = document.getElementsByClassName('dots1')[0];
-const dots2 = document.getElementsByClassName('dots2')[0];
-const dots3 = document.getElementsByClassName('dots3')[0];
-const img1 = document.getElementsByClassName('img1')[0];
-const img2 = document.getElementsByClassName('img2')[0];
-const img3 = document.getElementsByClassName('img3')[0];
-const header = document.getElementById('header');
+const hamburger = document.getElementsByClassName("fa-hamburger")[0];
+const nav = document.getElementsByTagName("nav")[0];
+const a = document.querySelectorAll("a");
+const dots1 = document.getElementsByClassName("dots1")[0];
+const dots2 = document.getElementsByClassName("dots2")[0];
+const dots3 = document.getElementsByClassName("dots3")[0];
+const img1 = document.getElementsByClassName("img1")[0];
+const img2 = document.getElementsByClassName("img2")[0];
+const img3 = document.getElementsByClassName("img3")[0];
+const header = document.getElementById("header");
 
-
-hamburger.addEventListener('click', function() {
-    this.classList.toggle('fa-times');
-    nav.classList.toggle('nav-toggle');
+hamburger.addEventListener("click", function () {
+  this.classList.toggle("fa-times");
+  nav.classList.toggle("nav-toggle");
 });
 
-for(let i=0 ; i<a.length ; i++){
-    a[i].addEventListener('click', function(){
-        hamburger.classList.remove('fa-times');
-        nav.classList.remove('nav-toggle');
-    });
+for (let i = 0; i < a.length; i++) {
+  a[i].addEventListener("click", function () {
+    hamburger.classList.remove("fa-times");
+    nav.classList.remove("nav-toggle");
+  });
 }
 
-dots1.addEventListener('click', function() {
-    img1.style.display = 'block';
-    img2.style.display = 'none';
-    img3.style.display = 'none';
+dots1.addEventListener("click", function () {
+  img1.style.display = "block";
+  img2.style.display = "none";
+  img3.style.display = "none";
 });
 
-dots2.addEventListener('click', function() {
-    img2.style.display = 'block';
-    img1.style.display = 'none';
-    img3.style.display = 'none';
+dots2.addEventListener("click", function () {
+  img2.style.display = "block";
+  img1.style.display = "none";
+  img3.style.display = "none";
 });
 
-dots3.addEventListener('click', function() {
-    img3.style.display = 'block';
-    img2.style.display = 'none';
-    img1.style.display = 'none';
+dots3.addEventListener("click", function () {
+  img3.style.display = "block";
+  img2.style.display = "none";
+  img1.style.display = "none";
 });
 
-window.onscroll = function() {scrollfunction()};
+window.onscroll = function () {
+  scrollfunction();
+};
 function scrollfunction() {
   if (document.body.scrollTop > 20 || document.documentElement.scrollTop > 20) {
-    document.getElementById('header').classList.add('header-active');
-  }else{
-    document.getElementById('header').classList.remove('header-active');
+    document.getElementById("header").classList.add("header-active");
+  } else {
+    document.getElementById("header").classList.remove("header-active");
   }
 }
-
---
+++ service-worker.js
@@ -0,0 +1,45 @@
+let cacheName = "chatal restaurant";
+let cacheFiles = [
+  "/",
+  "/index.html",
+  "/fallback.html",
+  "/styles/styles.css",
+  "/js/app.js",
+  "/manifest.json",
+];
+
+// start service worker to install
+self.addEventListener("install", function (e) {
+  e.waitUntil(
+    caches
+      .open(cacheName)
+      .then((cache) => {
+        console.log("caching assets...");
+        cache.addAll(cacheFiles);
+      })
+      .catch((err) => console.log("not caching", err))
+  );
+  self.skipWaiting();
+});
+
+// activate
+self.addEventListener("activate", function (e) {
+  self.clients.claim();
+  console.log("service work activated!!");
+});
+
+// serve cached content to work offline
+self.addEventListener("fetch", function (e) {
+  e.respondWith(
+    caches
+      .match(e.request)
+      .then((response) => {
+        return response || fetch(e.request);
+      })
+      .catch((e) => {
+        if (e.request.url.indexOf(".html") > -1) {
+          return caches.match("/fallback.html");
+        }
+      })
+  );
+});
