83eba0db12b6f7169ee906dc85b7a6ae61165058
Esrafil <57289402+EsrafilElahi@users.noreply.github.com>
2022-02-04 21:03:43 +0330

    change service worker path

--- js/app.js
+++ js/app.js
@@ -1,7 +1,7 @@
 window.onload = () => {
   if ("serviceWorker" in navigator) {
     navigator.serviceWorker
-      .register("../service-worker.js")
+      .register("/service-worker.js")
       .then((res) => console.log("service worker registered", res))
       .catch((err) => console.log("service worker not registered", err));
   }
